e75976dc4d85047917e263745092ffe4b16f2c8d
Administrator <200922702@qq.com>
2020-07-13 10:08:07 +0800

    修改api接口文档

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "prince-x/cmd"
 
 // @title prince-x API
 // @version 0.0.1
-// @description 基于Gin + Vue + Element UI的前后端分离权限管理系统的接口文档
+// @description 基于基于Gin + ant design  Pro V4的前后端分离权限管理系统的接口文档
 // @description 添加qq: 200922702 请备注，谢谢！
 // @license.name Zhang Pengxuan
 // @license.url https://www.baidu.com
